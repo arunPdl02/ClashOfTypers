37fdf6ada77d7c1eac76834207e69d65aae59a07
M-anan779 <manan.gupta060204@gmail.com>
2025-08-03 17:50:43 -0700

    Code clean up

--- messages.py
+++ messages.py
@@ -7,14 +7,10 @@
 # 2. Helper functions to format JSON messages
 
 # Message types:
-MSG_CLAIM_REQ = "claim_request"             # client claim request
-MSG_CLAIM_ACCEPTED = "claim_accepted"       # server claim acceptance response
-MSG_CLAIM_REJECTED = "claim_rejected"       # server claim rejection response
-MSG_VERIFY_SUCCESS = "verify_success"       # client verify string + WPM request
-MSG_INFORM_BREAKFAIL = "inform_breakfail"   # client inform server lock break fail 
-MSG_LOCK_CAPTURED = "lock_captured"         # 
-MSG_LOCK_REVOKED = "lock_revoked"
-MSG_GRID_UPDATE = "grid_update"     # broadcast all clients return game.get_grid() after: claim_accepted, lock_captured, lock_revoked 
+MSG_CLAIM_REQ = "claim_request"             # client claim request (lock_id, user_name)
+MSG_BREAK_REQ = "break_request"             # client break lock request (lock_id, user_string, wpm)
+MSG_GRID_UPDATE = "grid_update"             # broadcast all clients lock object (lock object, player dict)
+MSG_MOUSE_COORDS = "mouse_coords"           # client sends mouse coordinates to server
 
 # def make_message(type, **kwargs): return json.dumps(...)
 # def parse_message(data): return json.loads(...)
